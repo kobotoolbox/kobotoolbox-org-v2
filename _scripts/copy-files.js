ebc037f3c4781ede5c87adcb5639c0e7ebb9171f
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2022-03-15 13:41:24 +0100

    add videos to copy script

--- _scripts/copy-files.js
+++ _scripts/copy-files.js
@@ -1,19 +1,33 @@
+/**
+ * This script copies `_assets/images` and `_assets/videos` to `assets`
+ * directory, which is being used also by parcel and is not stored in git.
+ */
+
 const fs = require('fs');
 const path = require('path');
 
-const destDir = './assets/images/';
+const destImagesDir = './assets/images/';
+const destVideosDir = './assets/videos/';
 
 /**
  * This will remove existing images directory and create `assets/`
  * if it doesn't exist yet.
  */
 function cleanup() {
-  if (fs.existsSync(destDir)) {
-    fs.rmSync(destDir, {force: true, recursive: true});
+  // Wipe and create images directory
+  if (fs.existsSync(destImagesDir)) {
+    fs.rmSync(destImagesDir, {force: true, recursive: true});
+  }
+  if (!fs.existsSync(destImagesDir)) {
+    fs.mkdirSync(destImagesDir, {recursive: true});
   }
 
-  if (!fs.existsSync(destDir)) {
-    fs.mkdirSync(destDir, {recursive: true});
+  // Wipe and create videos directory
+  if (fs.existsSync(destVideosDir)) {
+    fs.rmSync(destVideosDir, {force: true, recursive: true});
+  }
+  if (!fs.existsSync(destVideosDir)) {
+    fs.mkdirSync(destVideosDir, {recursive: true});
   }
 
   console.info('Cleanup done');
@@ -32,9 +46,14 @@ function copyFolderSync(from, to) {
   });
 }
 
+function copyAssetsVideos() {
+  console.info('Copying videos from assets…');
+  copyFolderSync('./_assets/videos/', destVideosDir);
+}
+
 function copyAssetsImages() {
   console.info('Copying images from assets…');
-  copyFolderSync('./_assets/images/', destDir);
+  copyFolderSync('./_assets/images/', destImagesDir);
 }
 
 function copyCommonImages() {
@@ -45,19 +64,20 @@ function copyCommonImages() {
 
   console.info('Copying images from kobo-common…');
 
-  if (!fs.existsSync(destDir + 'common/')) {
-    fs.mkdirSync(destDir + 'common/', {recursive: true});
+  if (!fs.existsSync(destImagesDir + 'common/')) {
+    fs.mkdirSync(destImagesDir + 'common/', {recursive: true});
   }
 
   images.forEach((filename) => {
     fs.copyFileSync(
       `node_modules/kobo-common/src/images/kobo-logo/${filename}`,
-      `${destDir}/common/${filename}`
+      `${destImagesDir}/common/${filename}`
     );
   });
 }
 
 // FYI: this order is important:
 cleanup();
+copyAssetsVideos();
 copyAssetsImages();
 copyCommonImages();
