c311a373979241769d57d9a34946eb529d89a02b
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2022-03-29 02:16:52 +0200

    human modal done
    plus a bit of general fixes

--- _assets/scripts/modal.ts
+++ _assets/scripts/modal.ts
@@ -13,6 +13,8 @@ export default class Modal {
   private contentEl;
   private closerEl;
 
+  private onKeyDownBound = this.onKeyDown.bind(this);
+
   constructor() {
     this.modalEl = document.querySelector(`*[${this.selectors.modal}]`);
     this.contentEl = document.querySelector(`*[${this.selectors.modalContent}]`);
@@ -58,12 +60,30 @@ export default class Modal {
     ) {
       this.contentEl.innerHTML = instanceContentEl.innerHTML;
     }
+
+    this.listenCloseOnEsc();
   }
 
   hideModal() {
     this.modalEl?.classList.remove(this.visibleClassName);
     if (this.contentEl !== null) {
       this.contentEl.innerHTML = '';
+    }
+
+    this.unlistenCloseOnEsc();
+  }
+
+  listenCloseOnEsc() {
+    document.addEventListener('keydown', this.onKeyDownBound);
+  }
+
+  unlistenCloseOnEsc() {
+    document.removeEventListener('keydown', this.onKeyDownBound);
+  }
+
+  onKeyDown(evt: KeyboardEvent) {
+    if (evt.key === 'Escape') {
+      this.hideModal();
     }
   }
 }
